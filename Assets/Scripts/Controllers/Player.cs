d0b802d224b0bed2b014f50420380e81d5da4a32
ACHILLESLAPTOP\tomas <tomasiac@sheridancollege.ca>
2026-09-17 23:43:08 -0400

    Task 1 A and B compelted

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -2,19 +2,22 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
-using UnityEngine.InputSystem.Utilities;
-using Unity.VisualScripting;
-using UnityEngine.UIElements;
+
 
 public class Player : MonoBehaviour
 {
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
-
+    // variables for task 1 A //
     public Vector2 bombOffset;// vector for offset
+    // Variables for task 1 B //
+    public float bombTrailSpacing; // used to space the bombs along the trail
+    public int numberOfTrailBombs; // used to determine the number of bombs in the trail
+
+    // used for the coroutine assignment provided in class //
     public float bombSpawnWaitTime = 3f; // time for the bomb to wait before spawning
-    public IEnumerator bombWaitCoroutine; // coroutine for the bomb
+    private IEnumerator bombWaitCoroutine; // coroutine for the bomb
     void Start()
     {
         Debug.Log(dotProduct(transform.up,enemyTransform.up));
@@ -23,22 +26,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bombOffset = transform.position + transform.up; // offet of the bomb
         // checks if the b key is pressed
         if (Keyboard.current.bKey.wasPressedThisFrame)
         {
-            bombWaitCoroutine = waitToSpawnBomb(bombSpawnWaitTime, bombOffset); // sets the bomb coroutine at the start of the game
-            SpawnBombAtOffset(bombOffset); // spawns a bomb
+            bombOffset = transform.position + transform.up; // offet of the bomb
+            SpawnBombAtOffset(bombOffset); // spawns bomb
+        }
+        // checks if the t key is pressed
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs); // spawns a trail of bombs behind the player
+        }
+        
+        }
+    
+    // used to spawn a trail of bombs behind the player
+    public void SpawnBombTrail(float inBombSpacing, int inNumberOfBombs)
+    {
+        Vector3 bombOffsetAmount = transform.position; // used locally to keep track of the distance between bombs
+        for(int i = 0; i < inNumberOfBombs; i++)
+        {
+            bombOffsetAmount.y -= inBombSpacing;
+            Instantiate(bombPrefab,bombOffsetAmount , Quaternion.identity);// spawns bomb
 
         }
     }
     // function that will be used to spawn bomb at an offset
-    public void SpawnBombAtOffset(Vector3 inOffset) { 
- 
-        StartCoroutine(bombWaitCoroutine); // only starts this isntance of the coroutine
-        
+    public void SpawnBombAtOffset(Vector3 inOffset) {
+            bombWaitCoroutine = waitToSpawnBomb(bombSpawnWaitTime, inOffset); // sets the bomb coroutine at the start of the game
+            StartCoroutine(bombWaitCoroutine); // only starts this isntance of the coroutine                                                              
     }
-    //coroutine that stops
+
+    // classwork methods //
+    //coroutine that is used before spawning bombs when b is pressed
     private IEnumerator waitToSpawnBomb(float waitTimer,Vector3 inOffset)
     {
 
@@ -46,13 +66,14 @@ public class Player : MonoBehaviour
         Instantiate(bombPrefab, inOffset, Quaternion.identity);// spawns bomb
         
     }
-
+    // calculation used to normalize a vector ///classwork////
     public Vector2 NormalizeCustom(Vector2 inVector)
     {
         float mag = inVector.magnitude;
         Vector2 normalize = new Vector2(inVector.x / Mathf.Abs(mag), inVector.y / Mathf.Abs(mag));
         return normalize;
     }
+    // calculation used to find the dot product of a vector set. used to find angle between two vectors //// claswork/////
     public float dotProduct(Vector2 inVector, Vector2 enemyVector)
     {
         float mag = inVector.magnitude;
@@ -60,6 +81,6 @@ public class Player : MonoBehaviour
         float enemyMag = enemyVector.magnitude;
         // dot product for finding the angle
         float dotprod = ((inVector.x * enemyVector.x) + (inVector.y * enemyVector.y)) / Mathf.Abs(mag) * Mathf.Abs(enemyMag);
-        return Mathf.Cos(dotprod); ;
+        return Mathf.Cos(dotprod); 
     }
 }
